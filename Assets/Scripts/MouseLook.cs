0104ab8f3fdd4d344ddbf5fb1320d24169358588
DekkoBraze <greende556@gmail.com>
2023-02-21 19:54:40 +0300

    Movement and camera

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes
+    {
+        MouseX = 0,
+        MouseY = 1
+    }
+
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+
+    public RotationAxes axes = RotationAxes.MouseX;
+
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+            float rotationY = transform.localEulerAngles.y;
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+    }
+
+    private void VerticalCameraLook()
+    {
+        _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+        float rotationY = transform.localEulerAngles.y;
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+    }
+}
